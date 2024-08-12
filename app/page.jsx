b8ca586ab7e835a602ea0b06038e1cc79db68e90
Adnan Jafri <adnan.jafri7@gmail.com>
2024-08-12 20:46:12 +0530

    added resume to download

--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,25 @@
+"use client";
 import Photo from "@/components/Photo"
 import Social from "@/components/Social"
 import Stats from "@/components/Stats"
 import { Button } from "@/components/ui/button"
 import {FiDownload} from 'react-icons/fi'
+// import my_resume from "../public/assets/resume/my_resume.pdf"
 const Home = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
         <div className="text-center xl:text-left order-2 xl:order-none">
-          <span className="text-xl">Software Developer</span>
+          <span className="text-xl">Mobile & Web Application Developer</span>
           <h1 className="h1 mb-6">
             {`Hello I'm` }<br/> <span className="text-accent">Adnan Jafri</span>
           </h1>
-          <p className="max-w-[500px] mb-9 text-white/80">
-          I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies.
+          <p className="max-w-[500px] mb-9 text-white/80 text-justify">
+          I excel at developing intuitive web and mobile applications and am proficient in a range of technologies, including React.js, React Native, HTML, CSS, and JavaScript.
           </p>
           <div className="flex flex-col xl:flex-row items-center gap-8 mb-8 xl:mb-8">
-            <Button size="lg" className="uppercase flex items-center gap-2">
+            <Button size="lg" className="uppercase flex items-center gap-2"  onClick={() => window.open('my_resume.pdf')}>
               <span>Download Resume</span>
               <FiDownload className="text-xl"/>
             </Button>
